a3bbd66633b874a04de810a1325528d02f14eeff
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2019-07-17 10:45:40 +0000

    Update dnie.sh

--- depends/packages/libXau.mk
+++ depends/packages/libXau.mk
@@ -7,4 +7,28 @@ $(package)_dependencies=xproto
 
 # When updating this package, check the default value of
 # --disable-xthreads. It is currently enabled.
-d
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared --disable-lint-library --without-lint
+  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
+  $(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf share lib/*.la
+endef
