8ee17dc2d1fa491f804e231b299cda15185d93d5
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2019-07-19 12:23:54 +0000

    commit file

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -8,4 +8,20 @@ $(package)_patches=dont_leak_info.patch
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
 $(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
-$(package)_build_o
+$(package)_build_opts_mingw32=-f Makefile.mingw
+$(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/dont_leak_info.patch
+endef
+
+define $(package)_build_cmds
+	$(MAKE) libminiupnpc.a $($(package)_build_opts)
+endef
+
+define $(package)_stage_cmds
+	mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib &&\
+	install *.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
+	install libminiupnpc.a $($(package)_staging_prefix_dir)/lib
+endef
