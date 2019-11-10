833a38452a1e1adbbe88b4cac62ba015ba34d06d
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2019-11-10 06:45:59 +0000

    logic error

--- src/crc32c/src/crc32c_sse42.cc
+++ src/crc32c/src/crc32c_sse42.cc
@@ -109,3 +109,151 @@ const uint32_t kBlock2SkipTable[8][16] = {
      0xf832bf3e, 0x939b3353, 0x2f61a7e4, 0x44c82b89},
     {0x00000000, 0xa6f1f0f6, 0x480f971d, 0xeefe67eb, 0x901f2e3a, 0x36eedecc,
      0xd810b927, 0x7ee149d1, 0x25d22a85, 0x8323da73, 0x6dddbd98, 0xcb2c4d6e,
+     0xb5cd04bf, 0x133cf449, 0xfdc293a2, 0x5b336354},
+    {0x00000000, 0x4ba4550a, 0x9748aa14, 0xdcecff1e, 0x2b7d22d9, 0x60d977d3,
+     0xbc3588cd, 0xf791ddc7, 0x56fa45b2, 0x1d5e10b8, 0xc1b2efa6, 0x8a16baac,
+     0x7d87676b, 0x36233261, 0xeacfcd7f, 0xa16b9875},
+    {0x00000000, 0xadf48b64, 0x5e056039, 0xf3f1eb5d, 0xbc0ac072, 0x11fe4b16,
+     0xe20fa04b, 0x4ffb2b2f, 0x7df9f615, 0xd00d7d71, 0x23fc962c, 0x8e081d48,
+     0xc1f33667, 0x6c07bd03, 0x9ff6565e, 0x3202dd3a},
+};
+
+constexpr const ptrdiff_t kPrefetchHorizon = 256;
+
+}  // namespace
+
+uint32_t ExtendSse42(uint32_t crc, const uint8_t* data, size_t size) {
+  const uint8_t* p = data;
+  const uint8_t* e = data + size;
+  uint32_t l = crc ^ kCRC32Xor;
+
+#define STEP1                  \
+  do {                         \
+    l = _mm_crc32_u8(l, *p++); \
+  } while (0)
+
+#define STEP4(crc)                             \
+  do {                                         \
+    crc = _mm_crc32_u32(crc, ReadUint32LE(p)); \
+    p += 4;                                    \
+  } while (0)
+
+#define STEP8(crc, data)                          \
+  do {                                            \
+    crc = _mm_crc32_u64(crc, ReadUint64LE(data)); \
+    data += 8;                                    \
+  } while (0)
+
+#define STEP8BY3(crc0, crc1, crc2, p0, p1, p2) \
+  do {                                         \
+    STEP8(crc0, p0);                           \
+    STEP8(crc1, p1);                           \
+    STEP8(crc2, p2);                           \
+  } while (0)
+
+#define STEP8X3(crc0, crc1, crc2, bs)                     \
+  do {                                                    \
+    crc0 = _mm_crc32_u64(crc0, ReadUint64LE(p));          \
+    crc1 = _mm_crc32_u64(crc1, ReadUint64LE(p + bs));     \
+    crc2 = _mm_crc32_u64(crc2, ReadUint64LE(p + 2 * bs)); \
+    p += 8;                                               \
+  } while (0)
+
+#define SKIP_BLOCK(crc, tab)                                      \
+  do {                                                            \
+    crc = tab[0][crc & 0xf] ^ tab[1][(crc >> 4) & 0xf] ^          \
+          tab[2][(crc >> 8) & 0xf] ^ tab[3][(crc >> 12) & 0xf] ^  \
+          tab[4][(crc >> 16) & 0xf] ^ tab[5][(crc >> 20) & 0xf] ^ \
+          tab[6][(crc >> 24) & 0xf] ^ tab[7][(crc >> 28) & 0xf];  \
+  } while (0)
+
+  // Point x at first 8-byte aligned byte in the buffer. This might be past the
+  // end of the buffer.
+  const uint8_t* x = RoundUp<8>(p);
+  if (x <= e) {
+    // Process bytes p is 8-byte aligned.
+    while (p != x) {
+      STEP1;
+    }
+  }
+
+  // Proccess the data in predetermined block sizes with tables for quickly
+  // combining the checksum. Experimentally it's better to use larger block
+  // sizes where possible so use a hierarchy of decreasing block sizes.
+  uint64_t l64 = l;
+  while ((e - p) >= kGroups * kBlock0Size) {
+    uint64_t l641 = 0;
+    uint64_t l642 = 0;
+    for (int i = 0; i < kBlock0Size; i += 8 * 8) {
+      // Prefetch ahead to hide latency.
+      RequestPrefetch(p + kPrefetchHorizon);
+      RequestPrefetch(p + kBlock0Size + kPrefetchHorizon);
+      RequestPrefetch(p + 2 * kBlock0Size + kPrefetchHorizon);
+
+      // Process 64 bytes at a time.
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+      STEP8X3(l64, l641, l642, kBlock0Size);
+    }
+
+    // Combine results.
+    SKIP_BLOCK(l64, kBlock0SkipTable);
+    l64 ^= l641;
+    SKIP_BLOCK(l64, kBlock0SkipTable);
+    l64 ^= l642;
+    p += (kGroups - 1) * kBlock0Size;
+  }
+  while ((e - p) >= kGroups * kBlock1Size) {
+    uint64_t l641 = 0;
+    uint64_t l642 = 0;
+    for (int i = 0; i < kBlock1Size; i += 8) {
+      STEP8X3(l64, l641, l642, kBlock1Size);
+    }
+    SKIP_BLOCK(l64, kBlock1SkipTable);
+    l64 ^= l641;
+    SKIP_BLOCK(l64, kBlock1SkipTable);
+    l64 ^= l642;
+    p += (kGroups - 1) * kBlock1Size;
+  }
+  while ((e - p) >= kGroups * kBlock2Size) {
+    uint64_t l641 = 0;
+    uint64_t l642 = 0;
+    for (int i = 0; i < kBlock2Size; i += 8) {
+      STEP8X3(l64, l641, l642, kBlock2Size);
+    }
+    SKIP_BLOCK(l64, kBlock2SkipTable);
+    l64 ^= l641;
+    SKIP_BLOCK(l64, kBlock2SkipTable);
+    l64 ^= l642;
+    p += (kGroups - 1) * kBlock2Size;
+  }
+
+  // Process bytes 16 at a time
+  while ((e - p) >= 16) {
+    STEP8(l64, p);
+    STEP8(l64, p);
+  }
+
+  l = static_cast<uint32_t>(l64);
+  // Process the last few bytes.
+  while (p != e) {
+    STEP1;
+  }
+#undef SKIP_BLOCK
+#undef STEP8X3
+#undef STEP8BY3
+#undef STEP8
+#undef STEP4
+#undef STEP1
+
+  return l ^ kCRC32Xor;
+}
+
+}  // namespace crc32c
+
+#endif  // HAVE_SSE42 && (defined(_M_X64) || defined(__x86_64__))
