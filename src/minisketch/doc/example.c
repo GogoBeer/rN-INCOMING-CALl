b3b63971876d9ec0d8aafa8f4f4990cd655c68b7
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-01-20 22:50:51 +0000

    mtd: map: fixed bug in 64-bit systems

--- src/minisketch/doc/example.c
+++ src/minisketch/doc/example.c
@@ -19,4 +19,33 @@ int main(void) {
   size_t sersize = minisketch_serialized_size(sketch_a);
   assert(sersize == 12 * 4 / 8); // 4 12-bit values is 6 bytes.
   unsigned char *buffer_a = malloc(sersize);
-  minisketch_serialize(
+  minisketch_serialize(sketch_a, buffer_a);
+  minisketch_destroy(sketch_a);
+
+  minisketch *sketch_b = minisketch_create(12, 0, 4); // Bob's own sketch
+  for (int i = 3002; i < 3012; ++i) {
+    minisketch_add_uint64(sketch_b, i);
+  }
+
+  sketch_a = minisketch_create(12, 0, 4);     // Alice's sketch
+  minisketch_deserialize(sketch_a, buffer_a); // Load Alice's sketch
+  free(buffer_a);
+
+  // Merge the elements from sketch_a into sketch_b. The result is a sketch_b
+  // which contains all elements that occurred in Alice's or Bob's sets, but not
+  // in both.
+  minisketch_merge(sketch_b, sketch_a);
+
+  uint64_t differences[4];
+  ssize_t num_differences = minisketch_decode(sketch_b, 4, differences);
+  minisketch_destroy(sketch_a);
+  minisketch_destroy(sketch_b);
+  if (num_differences < 0) {
+    printf("More than 4 differences!\n");
+  } else {
+    ssize_t i;
+    for (i = 0; i < num_differences; ++i) {
+      printf("%u is in only one of the two sets\n", (unsigned)differences[i]);
+    }
+  }
+}
---
+++ src/minisketch/doc/moduli.md
@@ -0,0 +1,13 @@
+These are the irreducible polynomials over *GF(2)* used to represent field elements:
+
+* *x<sup>2</sup> + x + 1*
+* *x<sup>3</sup> + x + 1*
+* *x<sup>4</sup> + x + 1*
+* *x<sup>5</sup> + x<sup>2</sup> + 1*
+* *x<sup>6</sup> + x + 1*
+* *x<sup>7</sup> + x + 1*
+* *x<sup>8</sup> + x<sup>4</sup> + x<sup>3</sup> + x + 1*
+* *x<sup>9</sup> + x + 1*
+* *x<sup>10</sup> + x<sup>3</sup> + 1*
+* *x<sup>11</sup> + x<sup>2</sup> + 1*
+* *x<sup>12<
