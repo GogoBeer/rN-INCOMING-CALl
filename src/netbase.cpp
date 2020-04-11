5efb8cae64b35a1acf6bad9166661fb06f1af6b9
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-04-11 09:38:06 +0000

    Automated merge with ssh://jpgodine@jfxsrc.us.oracle.com//javafx/8.0/MASTER/jfx//rt

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -145,4 +145,233 @@ static bool LookupIntern(const std::string& name, std::vector<CNetAddr>& vIP, un
         // From our perspective, onion addresses are not hostnames but rather
         // direct encodings of CNetAddr much like IPv4 dotted-decimal notation
         // or IPv6 colon-separated hextet notation. Since we can't use
-        // getaddrinfo 
+        // getaddrinfo to decode them and it wouldn't make sense to resolve
+        // them, we return a network address representing it instead. See
+        // CNetAddr::SetSpecial(const std::string&) for more details.
+        if (addr.SetSpecial(name)) {
+            vIP.push_back(addr);
+            return true;
+        }
+    }
+
+    for (const CNetAddr& resolved : dns_lookup_function(name, fAllowLookup)) {
+        if (nMaxSolutions > 0 && vIP.size() >= nMaxSolutions) {
+            break;
+        }
+        /* Never allow resolving to an internal address. Consider any such result invalid */
+        if (!resolved.IsInternal()) {
+            vIP.push_back(resolved);
+        }
+    }
+
+    return (vIP.size() > 0);
+}
+
+bool LookupHost(const std::string& name, std::vector<CNetAddr>& vIP, unsigned int nMaxSolutions, bool fAllowLookup, DNSLookupFn dns_lookup_function)
+{
+    if (!ValidAsCString(name)) {
+        return false;
+    }
+    std::string strHost = name;
+    if (strHost.empty())
+        return false;
+    if (strHost.front() == '[' && strHost.back() == ']') {
+        strHost = strHost.substr(1, strHost.size() - 2);
+    }
+
+    return LookupIntern(strHost, vIP, nMaxSolutions, fAllowLookup, dns_lookup_function);
+}
+
+bool LookupHost(const std::string& name, CNetAddr& addr, bool fAllowLookup, DNSLookupFn dns_lookup_function)
+{
+    if (!ValidAsCString(name)) {
+        return false;
+    }
+    std::vector<CNetAddr> vIP;
+    LookupHost(name, vIP, 1, fAllowLookup, dns_lookup_function);
+    if(vIP.empty())
+        return false;
+    addr = vIP.front();
+    return true;
+}
+
+bool Lookup(const std::string& name, std::vector<CService>& vAddr, uint16_t portDefault, bool fAllowLookup, unsigned int nMaxSolutions, DNSLookupFn dns_lookup_function)
+{
+    if (name.empty() || !ValidAsCString(name)) {
+        return false;
+    }
+    uint16_t port{portDefault};
+    std::string hostname;
+    SplitHostPort(name, port, hostname);
+
+    std::vector<CNetAddr> vIP;
+    bool fRet = LookupIntern(hostname, vIP, nMaxSolutions, fAllowLookup, dns_lookup_function);
+    if (!fRet)
+        return false;
+    vAddr.resize(vIP.size());
+    for (unsigned int i = 0; i < vIP.size(); i++)
+        vAddr[i] = CService(vIP[i], port);
+    return true;
+}
+
+bool Lookup(const std::string& name, CService& addr, uint16_t portDefault, bool fAllowLookup, DNSLookupFn dns_lookup_function)
+{
+    if (!ValidAsCString(name)) {
+        return false;
+    }
+    std::vector<CService> vService;
+    bool fRet = Lookup(name, vService, portDefault, fAllowLookup, 1, dns_lookup_function);
+    if (!fRet)
+        return false;
+    addr = vService[0];
+    return true;
+}
+
+CService LookupNumeric(const std::string& name, uint16_t portDefault, DNSLookupFn dns_lookup_function)
+{
+    if (!ValidAsCString(name)) {
+        return {};
+    }
+    CService addr;
+    // "1.2:345" will fail to resolve the ip, but will still set the port.
+    // If the ip fails to resolve, re-init the result.
+    if(!Lookup(name, addr, portDefault, false, dns_lookup_function))
+        addr = CService();
+    return addr;
+}
+
+/** SOCKS version */
+enum SOCKSVersion: uint8_t {
+    SOCKS4 = 0x04,
+    SOCKS5 = 0x05
+};
+
+/** Values defined for METHOD in RFC1928 */
+enum SOCKS5Method: uint8_t {
+    NOAUTH = 0x00,        //!< No authentication required
+    GSSAPI = 0x01,        //!< GSSAPI
+    USER_PASS = 0x02,     //!< Username/password
+    NO_ACCEPTABLE = 0xff, //!< No acceptable methods
+};
+
+/** Values defined for CMD in RFC1928 */
+enum SOCKS5Command: uint8_t {
+    CONNECT = 0x01,
+    BIND = 0x02,
+    UDP_ASSOCIATE = 0x03
+};
+
+/** Values defined for REP in RFC1928 */
+enum SOCKS5Reply: uint8_t {
+    SUCCEEDED = 0x00,        //!< Succeeded
+    GENFAILURE = 0x01,       //!< General failure
+    NOTALLOWED = 0x02,       //!< Connection not allowed by ruleset
+    NETUNREACHABLE = 0x03,   //!< Network unreachable
+    HOSTUNREACHABLE = 0x04,  //!< Network unreachable
+    CONNREFUSED = 0x05,      //!< Connection refused
+    TTLEXPIRED = 0x06,       //!< TTL expired
+    CMDUNSUPPORTED = 0x07,   //!< Command not supported
+    ATYPEUNSUPPORTED = 0x08, //!< Address type not supported
+};
+
+/** Values defined for ATYPE in RFC1928 */
+enum SOCKS5Atyp: uint8_t {
+    IPV4 = 0x01,
+    DOMAINNAME = 0x03,
+    IPV6 = 0x04,
+};
+
+/** Status codes that can be returned by InterruptibleRecv */
+enum class IntrRecvError {
+    OK,
+    Timeout,
+    Disconnected,
+    NetworkError,
+    Interrupted
+};
+
+/**
+ * Try to read a specified number of bytes from a socket. Please read the "see
+ * also" section for more detail.
+ *
+ * @param data The buffer where the read bytes should be stored.
+ * @param len The number of bytes to read into the specified buffer.
+ * @param timeout The total timeout in milliseconds for this read.
+ * @param sock The socket (has to be in non-blocking mode) from which to read bytes.
+ *
+ * @returns An IntrRecvError indicating the resulting status of this read.
+ *          IntrRecvError::OK only if all of the specified number of bytes were
+ *          read.
+ *
+ * @see This function can be interrupted by calling InterruptSocks5(bool).
+ *      Sockets can be made non-blocking with SetSocketNonBlocking(const
+ *      SOCKET&, bool).
+ */
+static IntrRecvError InterruptibleRecv(uint8_t* data, size_t len, int timeout, const Sock& sock)
+{
+    int64_t curTime = GetTimeMillis();
+    int64_t endTime = curTime + timeout;
+    while (len > 0 && curTime < endTime) {
+        ssize_t ret = sock.Recv(data, len, 0); // Optimistically try the recv first
+        if (ret > 0) {
+            len -= ret;
+            data += ret;
+        } else if (ret == 0) { // Unexpected disconnection
+            return IntrRecvError::Disconnected;
+        } else { // Other error or blocking
+            int nErr = WSAGetLastError();
+            if (nErr == WSAEINPROGRESS || nErr == WSAEWOULDBLOCK || nErr == WSAEINVAL) {
+                // Only wait at most MAX_WAIT_FOR_IO at a time, unless
+                // we're approaching the end of the specified total timeout
+                const auto remaining = std::chrono::milliseconds{endTime - curTime};
+                const auto timeout = std::min(remaining, std::chrono::milliseconds{MAX_WAIT_FOR_IO});
+                if (!sock.Wait(timeout, Sock::RECV)) {
+                    return IntrRecvError::NetworkError;
+                }
+            } else {
+                return IntrRecvError::NetworkError;
+            }
+        }
+        if (interruptSocks5Recv)
+            return IntrRecvError::Interrupted;
+        curTime = GetTimeMillis();
+    }
+    return len == 0 ? IntrRecvError::OK : IntrRecvError::Timeout;
+}
+
+/** Convert SOCKS5 reply to an error message */
+static std::string Socks5ErrorString(uint8_t err)
+{
+    switch(err) {
+        case SOCKS5Reply::GENFAILURE:
+            return "general failure";
+        case SOCKS5Reply::NOTALLOWED:
+            return "connection not allowed";
+        case SOCKS5Reply::NETUNREACHABLE:
+            return "network unreachable";
+        case SOCKS5Reply::HOSTUNREACHABLE:
+            return "host unreachable";
+        case SOCKS5Reply::CONNREFUSED:
+            return "connection refused";
+        case SOCKS5Reply::TTLEXPIRED:
+            return "TTL expired";
+        case SOCKS5Reply::CMDUNSUPPORTED:
+            return "protocol error";
+        case SOCKS5Reply::ATYPEUNSUPPORTED:
+            return "address type not supported";
+        default:
+            return "unknown";
+    }
+}
+
+bool Socks5(const std::string& strDest, uint16_t port, const ProxyCredentials* auth, const Sock& sock)
+{
+    IntrRecvError recvr;
+    LogPrint(BCLog::NET, "SOCKS5 connecting %s\n", strDest);
+    if (strDest.size() > 255) {
+        return error("Hostname too long");
+    }
+    // Construct the version identifier/method selection message
+    std::vector<uint8_t> vSocks5Init;
+    vSocks5Init.push_back(SOCKSVersion::SOCKS5); // We want the SOCK5 protocol
+    if (auth) {
