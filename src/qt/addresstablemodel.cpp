c7914642d316274a4f6d418b4e8c7c84951b4d0f
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-05-09 15:46:34 +0000

    Updated test suite

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -1,3 +1,241 @@
 // Copyright (c) 2011-2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
-// file 
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/addresstablemodel.h>
+
+#include <qt/guiutil.h>
+#include <qt/walletmodel.h>
+
+#include <key_io.h>
+#include <wallet/wallet.h>
+
+#include <algorithm>
+
+#include <QFont>
+#include <QDebug>
+
+const QString AddressTableModel::Send = "S";
+const QString AddressTableModel::Receive = "R";
+
+struct AddressTableEntry
+{
+    enum Type {
+        Sending,
+        Receiving,
+        Hidden /* QSortFilterProxyModel will filter these out */
+    };
+
+    Type type;
+    QString label;
+    QString address;
+
+    AddressTableEntry() {}
+    AddressTableEntry(Type _type, const QString &_label, const QString &_address):
+        type(_type), label(_label), address(_address) {}
+};
+
+struct AddressTableEntryLessThan
+{
+    bool operator()(const AddressTableEntry &a, const AddressTableEntry &b) const
+    {
+        return a.address < b.address;
+    }
+    bool operator()(const AddressTableEntry &a, const QString &b) const
+    {
+        return a.address < b;
+    }
+    bool operator()(const QString &a, const AddressTableEntry &b) const
+    {
+        return a < b.address;
+    }
+};
+
+/* Determine address type from address purpose */
+static AddressTableEntry::Type translateTransactionType(const QString &strPurpose, bool isMine)
+{
+    AddressTableEntry::Type addressType = AddressTableEntry::Hidden;
+    // "refund" addresses aren't shown, and change addresses aren't returned by getAddresses at all.
+    if (strPurpose == "send")
+        addressType = AddressTableEntry::Sending;
+    else if (strPurpose == "receive")
+        addressType = AddressTableEntry::Receiving;
+    else if (strPurpose == "unknown" || strPurpose == "") // if purpose not set, guess
+        addressType = (isMine ? AddressTableEntry::Receiving : AddressTableEntry::Sending);
+    return addressType;
+}
+
+// Private implementation
+class AddressTablePriv
+{
+public:
+    QList<AddressTableEntry> cachedAddressTable;
+    AddressTableModel *parent;
+
+    explicit AddressTablePriv(AddressTableModel *_parent):
+        parent(_parent) {}
+
+    void refreshAddressTable(interfaces::Wallet& wallet, bool pk_hash_only = false)
+    {
+        cachedAddressTable.clear();
+        {
+            for (const auto& address : wallet.getAddresses())
+            {
+                if (pk_hash_only && !std::holds_alternative<PKHash>(address.dest)) {
+                    continue;
+                }
+                AddressTableEntry::Type addressType = translateTransactionType(
+                        QString::fromStdString(address.purpose), address.is_mine);
+                cachedAddressTable.append(AddressTableEntry(addressType,
+                                  QString::fromStdString(address.name),
+                                  QString::fromStdString(EncodeDestination(address.dest))));
+            }
+        }
+        // std::lower_bound() and std::upper_bound() require our cachedAddressTable list to be sorted in asc order
+        // Even though the map is already sorted this re-sorting step is needed because the originating map
+        // is sorted by binary address, not by base58() address.
+        std::sort(cachedAddressTable.begin(), cachedAddressTable.end(), AddressTableEntryLessThan());
+    }
+
+    void updateEntry(const QString &address, const QString &label, bool isMine, const QString &purpose, int status)
+    {
+        // Find address / label in model
+        QList<AddressTableEntry>::iterator lower = std::lower_bound(
+            cachedAddressTable.begin(), cachedAddressTable.end(), address, AddressTableEntryLessThan());
+        QList<AddressTableEntry>::iterator upper = std::upper_bound(
+            cachedAddressTable.begin(), cachedAddressTable.end(), address, AddressTableEntryLessThan());
+        int lowerIndex = (lower - cachedAddressTable.begin());
+        int upperIndex = (upper - cachedAddressTable.begin());
+        bool inModel = (lower != upper);
+        AddressTableEntry::Type newEntryType = translateTransactionType(purpose, isMine);
+
+        switch(status)
+        {
+        case CT_NEW:
+            if(inModel)
+            {
+                qWarning() << "AddressTablePriv::updateEntry: Warning: Got CT_NEW, but entry is already in model";
+                break;
+            }
+            parent->beginInsertRows(QModelIndex(), lowerIndex, lowerIndex);
+            cachedAddressTable.insert(lowerIndex, AddressTableEntry(newEntryType, label, address));
+            parent->endInsertRows();
+            break;
+        case CT_UPDATED:
+            if(!inModel)
+            {
+                qWarning() << "AddressTablePriv::updateEntry: Warning: Got CT_UPDATED, but entry is not in model";
+                break;
+            }
+            lower->type = newEntryType;
+            lower->label = label;
+            parent->emitDataChanged(lowerIndex);
+            break;
+        case CT_DELETED:
+            if(!inModel)
+            {
+                qWarning() << "AddressTablePriv::updateEntry: Warning: Got CT_DELETED, but entry is not in model";
+                break;
+            }
+            parent->beginRemoveRows(QModelIndex(), lowerIndex, upperIndex-1);
+            cachedAddressTable.erase(lower, upper);
+            parent->endRemoveRows();
+            break;
+        }
+    }
+
+    int size()
+    {
+        return cachedAddressTable.size();
+    }
+
+    AddressTableEntry *index(int idx)
+    {
+        if(idx >= 0 && idx < cachedAddressTable.size())
+        {
+            return &cachedAddressTable[idx];
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+};
+
+AddressTableModel::AddressTableModel(WalletModel *parent, bool pk_hash_only) :
+    QAbstractTableModel(parent), walletModel(parent)
+{
+    columns << tr("Label") << tr("Address");
+    priv = new AddressTablePriv(this);
+    priv->refreshAddressTable(parent->wallet(), pk_hash_only);
+}
+
+AddressTableModel::~AddressTableModel()
+{
+    delete priv;
+}
+
+int AddressTableModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0;
+    }
+    return priv->size();
+}
+
+int AddressTableModel::columnCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0;
+    }
+    return columns.length();
+}
+
+QVariant AddressTableModel::data(const QModelIndex &index, int role) const
+{
+    if(!index.isValid())
+        return QVariant();
+
+    AddressTableEntry *rec = static_cast<AddressTableEntry*>(index.internalPointer());
+
+    const auto column = static_cast<ColumnIndex>(index.column());
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
+        switch (column) {
+        case Label:
+            if (rec->label.isEmpty() && role == Qt::DisplayRole) {
+                return tr("(no label)");
+            } else {
+                return rec->label;
+            }
+        case Address:
+            return rec->address;
+        } // no default case, so the compiler can warn about missing cases
+        assert(false);
+    } else if (role == Qt::FontRole) {
+        switch (column) {
+        case Label:
+            return QFont();
+        case Address:
+            return GUIUtil::fixedPitchFont();
+        } // no default case, so the compiler can warn about missing cases
+        assert(false);
+    } else if (role == TypeRole) {
+        switch(rec->type)
+        {
+        case AddressTableEntry::Sending:
+            return Send;
+        case AddressTableEntry::Receiving:
+            return Receive;
+        case AddressTableEntry::Hidden:
+            return {};
+        } // no default case, so the compiler can warn about missing cases
+        assert(false);
+    }
+    return QVariant();
+}
+
+bool AddressTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if(!index.isValid())
+        return false;
+    AddressTableEntry
