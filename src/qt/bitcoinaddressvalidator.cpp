071f8b92b0c440fb1504fc4d0ecd09fa9d67c628
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-05-16 17:06:38 +0000

    msm: mdss: update max fence wait timeout for all acquire fence

---
+++ src/qt/bitcoinaddressvalidator.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/bitcoinaddressvalidator.h>
+
+#include <key_io.h>
+
+/* Base58 characters are:
+     "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+  This is:
+  - All numbers except for '0'
+  - All upper-case letters except for 'I' and 'O'
+  - All lower-case letters except for 'l'
+*/
+
+BitcoinAddressEntryValidator::BitcoinAddressEntryValidator(QObject *parent) :
+    QValidator(
