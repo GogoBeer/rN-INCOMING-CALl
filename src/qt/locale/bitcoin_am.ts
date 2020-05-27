ebd2ae8a113fdcf93e842e1e150916555d27e0b7
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-05-27 19:09:22 +0000

    a test

---
+++ src/qt/locale/bitcoin_am.ts
@@ -0,0 +1,136 @@
+<TS language="am" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>አድራሻ ወይም መለያ ስም ለመቀየር ቀኙን ጠቅ ያድርጉ</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>አዲስ አድራሻ ፍጠር</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;አዲስ</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>አሁን የተመረጠውን አድራሻ ወደ ሲስተሙ ቅንጥብ ሰሌዳ ቅዳ</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;ቅዳ</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>ዝጋ</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>አሁን የተመረጠውን አድራሻ ከዝርዝሩ ውስጥ ሰርዝ</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>በአሁኑ ማውጫ ውስጥ ያለውን መረጃ ወደ አንድ ፋይል ላክ</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;ላክ</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;ሰርዝ</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>ገንዘብ/ኮይኖች የሚልኩለትን አድራሻ ይምረጡ</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>ገንዘብ/ኮይኖች የሚቀበሉበትን አድራሻ ይምረጡ</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>ምረጥ</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>የመላኪያ አድራሻዎች</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>የመቀበያ አድራሻዎች</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>እነኚ የቢትኮይን ክፍያ የመላኪያ አድራሻዎችዎ ናቸው:: ገንዘብ/ኮይኖች ከመላክዎ በፊት መጠኑን እና የመቀበያ አድራሻውን ሁልጊዜ ያረጋግጡ::</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;አድራሻ ቅዳ</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>ቅዳ &amp;መለያ ስም</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation> &amp;ቀይር</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>የአድራሻ ዝርዝር ላክ</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>ኮማ ሴፓሬትድ ፋይል (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>ወደ ውጪ መላክ አልተሳካም</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>የአድራሻ ዝርዝሩን ወደ %1 ለማስቀመጥ ሲሞከር ስህተት አጋጥሟል:: እባክዎ መልሰው ይሞክሩ::</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>መለያ ስም</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>አድራሻ</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(መለያ ስም የለም)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>የይለፍ-ሐረግ ንግግር</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>የይለፍ-ሐረግዎን ያስገቡ</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>አዲስ የይለፍ-ሐረግ</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>አዲስ የይለፍ-ሐረጉን ይድገሙት</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>የቢትኮይን ቦርሳውን አመስጥር</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>ይህ ክንዋኔ የቢትኮይን ቦርሳዎን ለመክፈት የቦርሳዎ ይለፍ-ሐረግ ያስፈልገዋል::</t
