5a6576b70f611397eb998be0c004fa2326c5667f
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-05-29 21:09:12 +0000

    Update HISTORY

--- src/qt/locale/bitcoin_be.ts
+++ src/qt/locale/bitcoin_be.ts
@@ -107,3 +107,207 @@
     <message>
         <source>Address</source>
         <translation>Адрас</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>непазначаны</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Дыялог сакрэтнай фразы</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Увядзіце кодавую фразу</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Новая кодавая фраза</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Паўтарыце новую кодавую фразу</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Зашыфраваць гаманец.</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Гэтая аперацыя патрабуе кодавую фразу, каб рзблакаваць гаманец.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Разблакаваць гаманец</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Гэтая аперацыя патрабуе пароль каб расшыфраваць гаманец.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Рачшыфраваць гаманец</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Змяніць пароль</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Пацвердзіце шыфраванне гаманца</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Увага: калі вы зашыфруеце свой гаманец і страціце парольную фразу, то &lt;b&gt;СТРАЦІЦЕ ЎСЕ СВАЕ БІТКОЙНЫ&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Ці ўпэўненыя вы, што жадаеце зашыфраваць свой гаманец?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Гаманец зашыфраваны</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>ВАЖНА: Усе папярэднія копіі гаманца варта замяніць новым зашыфраваным файлам. У мэтах бяспекі папярэднія копіі незашыфраванага файла-гаманца стануць неўжывальнымі, калі вы станеце карыстацца новым зашыфраваным гаманцом.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Шыфраванне гаманца няўдалае</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Шыфраванне гаманца не адбылося з-за ўнутранай памылкі. Гаманец незашыфраваны.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Уведдзеныя паролі не супадаюць</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Разблакаванне гаманца няўдалае</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Уведзены пароль  для расшыфравання гаманца памылковы</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Расшыфраванне гаманца няўдалае</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Парольная фраза гаманца паспяхова зменена.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Увага: Caps Lock уключаны!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    </context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Падпісаць паведамленне...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Сінхранізацыя з сецівам...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>Агляд</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Паказвае агульныя звесткі аб гаманцы</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>Транзакцыі</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Праглядзець гісторыю транзакцый</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Выйсці</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Выйсці з праграмы</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Аб Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Паказаць інфармацыю аб Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>Опцыі...</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>Зашыфраваць Гаманец...</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>Стварыць копію гаманца...</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>&amp;Change Passphrase...</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Адчыниць &amp;URI...</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>Пераіндэксацыя блокаў на дыску...</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>Даслаць манеты на Біткойн-адрас</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Зрабіце копію гаманца ў іншае месца</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>Змяніць пароль шыфравання гаманца</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>Праверыць паведамленне...</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>Даслаць</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>Атрымаць</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>&amp;Паказаць / Схаваць</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>Паказаць альбо схаваць галоўнае вакно</translation>
+    </message>
+    <message>
+        <source>Encrypt the private key
