c3d03ba3e9ee9fc2c74ed45db898620d70a1ef32
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-06-10 10:02:08 +0000

    Imagenes

--- src/qt/locale/bitcoin_cy.ts
+++ src/qt/locale/bitcoin_cy.ts
@@ -173,4 +173,210 @@
     </message>
     <message>
         <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
-        <tra
+        <translation>PWYSIG: Mi ddylai unrhyw back ups blaenorol rydych wedi ei wneud o ffeil eich waled gael ei ddiweddaru efo'r ffeil amgryptiedig newydd ei chreu. Am resymau diogelwch, bydd back ups blaenorol o ffeil y walet heb amgryptio yn ddiwerth mor fuan ac yr ydych yn dechrau defnyddio'r waled amgryptiedig newydd.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Amgryptio waled wedi methu</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Amgryptio waled wedi methu oherwydd gwall mewnol. Dydi eich waled heb amgryptio.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Nid ydi'r cyfrineiriau a gyflenwyd yn cyfateb.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Dadgloi waled wedi methu</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Mae'r cyfrinair ysgrifennwyd ar gyfer datgryptio'r waled yn anghywir.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Amgryptio waled wedi methu</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Newid cyfrinair waled yn llwyddiannus.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Rhybudd: Mae allwedd Caps Lock ymlaen!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Rhwydfwgwd</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Gwaharddwyd Nes</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Arwyddo &amp;neges...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Cysoni â'r rhwydwaith...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Trosolwg</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Dangos trosolwg cyffredinol y waled</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Trafodion</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Pori hanes trafodion</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>A&amp;llanfa</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Gadael rhaglen</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>&amp;Ynghylch %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>Dangos gwybodaeth am %1</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Ynghylch &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Dangos gwybodaeth am Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;Opsiynau</translation>
+    </message>
+    <message>
+        <source>Modify configuration options for %1</source>
+        <translation>Addasu ffurfweddiad dewisiadau ar gyfer %1</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>&amp;Amgryptio'r waled...</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>&amp;Backup Waled...</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>&amp;Newid cyfrinymadrodd...</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Agor &amp;URI...</translation>
+    </message>
+    <message>
+        <source>Wallet:</source>
+        <translation>Waled:</translation>
+    </message>
+    <message>
+        <source>Click to disable network activity.</source>
+        <translation>Cliciwch i anablu gweithgaredd y rhwydwaith.</translation>
+    </message>
+    <message>
+        <source>Network activity disabled.</source>
+        <translation>Gweithgaredd rhwydwaith wedi anablu.</translation>
+    </message>
+    <message>
+        <source>Click to enable network activity again.</source>
+        <translation>Cliciwch i alluogi gweithgaredd y rhwydwaith eto.</translation>
+    </message>
+    <message>
+        <source>Syncing Headers (%1%)...</source>
+        <translation>Syncio pennawdau (%1%)...</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>Ailfynegi y blociau ar ddisg...</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>Anfon arian i gyfeiriad Bitcoin</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Bacio fyny'r waled i leoliad arall</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>Newid y cyfrinair ddefnyddiwyd ar gyfer amgryptio'r waled</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>&amp;Gwirio neges...</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;Anfon</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;Derbyn</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>&amp;Dangos / Cuddio</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>Dangos neu guddio y brif Ffenest</translation>
+    </message>
+    <message>
+        <source>Encrypt the private keys that belong to your wallet</source>
+        <translation>Amgryptio'r allweddi preifat sy'n perthyn i'ch waled</translation>
+    </message>
+    <message>
+        <source>Sign messages with your Bitcoin addresses to prove you own them</source>
+        <translation>Arwyddo negeseuon gyda eich cyfeiriadau Bitcoin i brofi mae chi sy'n berchen arnynt</translation>
+    </message>
+    <message>
+        <source>Verify messages to ensure they were signed with specified Bitcoin addresses</source>
+        <translation>Gwirio negeseuon i sicrhau eu bod wedi eu harwyddo gyda cyfeiriadau Bitcoin penodol</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Ffeil</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Gosodiadau</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Cymorth</translation>
+    </message>
+    <message>
+        <source>Tabs toolbar</source>
+        <translation>Bar offer tabiau</translation>
+    </message>
+    <message>
+        <source>Request payments (generates QR codes and bitcoin: URIs)</source>
+        <transl
