2f193578473dbb70f332c971afdaad193120535a
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-09-04 20:35:15 +0000

    Update lists.js

--- src/qt/locale/bitcoin_eu.ts
+++ src/qt/locale/bitcoin_eu.ts
@@ -102,4 +102,196 @@
     <name>AddressTableModel</name>
     <message>
         <source>Label</source>
-        <translation>Izendape
+        <translation>Izendapen</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Helbidea</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(izendapenik ez)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Pasahitzaren dialogoa</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Pasahitza sartu</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Pasahitz berria</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Pasahitz berria errepiikatu</translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation>Pasahitza erakutsi</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Diruzorroa enkriptatu</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Diruzorroaren pasahitza behar du eragiketa honek, diruzorroa desblokeatzeko.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Diruzorroa desblokeatu</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Eragiketa honek zure diruzorroaren pasahitza behar du, diruzorroa desenkriptatzeko.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Diruzorroa desenkriptatu</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Pasahitza aldatu</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Diruorroaren enkriptazioa berretsi</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Abisua: Diruzorroa enkriptatzen baduzu eta zure pasahitza galtzen baduzu, &lt;b&gt;BITCOIN GUZTIAK GALDUKO DITUZU&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Seguru al zaude, zure diruzorroa enkriptatu nahi duzula?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Zorroa enkriptatuta</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase for the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Diruzorroaren pasahitz berria sartu. &lt;br/&gt;Mesedez &lt;b&gt; hamar edo gehiago ausazko hizkiko&lt;/b&gt; pasahitza erabili, edo &lt;b&gt; gutxienez zortzi hitz&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase for the wallet.</source>
+        <translation>Diruzorroaren pasahitz zahar zein berria sartu.</translation>
+    </message>
+    <message>
+        <source>Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>Gogoan izan diruzorroaren enkripzioak ezin dituela zure bitcoinak zure ordenagailuan izan dezakezun malware batengandik lapurtuak izatetik guztiz babestu .</translation>
+    </message>
+    <message>
+        <source>Wallet to be encrypted</source>
+        <translation>Enkriptatzeko diruzorroa</translation>
+    </message>
+    <message>
+        <source>Your wallet is about to be encrypted. </source>
+        <translation>Zure diruzorroa enkriptatzekotan dago</translation>
+    </message>
+    <message>
+        <source>Your wallet is now encrypted. </source>
+        <translation>Zure diruzorroa enkriptatua dago orain.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>GARRANTZITSUA: Diruzorroaren segurtasun-kopia, wallet.dat, erabilezina bihurtuko da enkriptazioa burutu ondoren. Ondorioz, segurtasun-kopia berriekin ordezkatu beharko zenituzke zure jada eginik dituzun diruzorroaren kopiak.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Diruzorroaren enkriptazioak huts egin du</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Diruzorroaren enkriptazioak huts egin du barne-akats baten ondorioz. Zure diruzorroa ez da enkriptatu.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Eman dituzun pasahitzak ez datoz bat.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Zorroaren desblokeoak huts egin du</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Zorroa desenkriptatzeko sartutako pasahitza okerra da.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Zorroaren desenkriptazioak huts egin du</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Diruzorroaren pasahitza arrakastaz aldatu da.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Abisua: Mayuskulak blokeatuak dituzu!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Saremaskara</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Honarte debekatua </translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>&amp;Mezua zinatu</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Sarearekin sinkronizatzen...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Gainbegiratu</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Diruzorroaren begirada orokorra ikusi</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Transakzioak</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Ikusi transakzioen historia</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Irten</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Aplikaziotik irten</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>%1-ri buruz</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>%1-ri buruzko informazioa azaldu</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>&amp;Qt-ri buruz</translation>
+    </message>
+    <message>
+        <s
