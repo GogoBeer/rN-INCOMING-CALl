3dfd3af6c4fec104d30f1816e93269b6ac6e78ee
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-09-11 08:43:49 +0000

    cs fix

---
+++ src/qt/locale/bitcoin_fil.ts
@@ -0,0 +1,108 @@
+<TS language="fil" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Mag-right-klik upang baguhin ang address o label</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Gumawa ng bagong address</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>Bago</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopyahin ang napiling address sa system clipboard</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>Kopyahin</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>Isara</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Burahin ang napiling address sa listahan</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>I-enter ang address o label upang maghanap</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>I-export ang data mula sa kasalukuyang tab sa isang file</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>I-export</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Burahin</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Piliin ang address kung saan ipapadala ang coins</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Piliin ang address na tatanggap ng coins</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>Pumili</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Mga address na padadalahan</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Mga address na tatanggap</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Ito ang iyong mga Bitcoin address para sa pagpapadala ng bayad. Laging suriin ang halaga at ang address na tatanggap bago magpadala ng coins.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>Kopyahin ang address</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopyahin ang Label</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>I-edit</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>I-export ang Listahan ng Address</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Comma separated file (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Nabigo ang Pag-export</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Mayroong error sa pag-save ng listahan ng address sa %1. Subukang muli.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Label</translation>
+    </message>
+    <message>
+        <source>Addre
