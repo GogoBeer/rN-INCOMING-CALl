bf79fc47109d73c431310863a0efef686c3d19f0
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-09-24 19:18:29 +0000

    - updated documentation

--- src/qt/locale/bitcoin_fil.ts
+++ src/qt/locale/bitcoin_fil.ts
@@ -105,4 +105,186 @@
         <translation>Label</translation>
     </message>
     <message>
-        <source>Addre
+        <source>Address</source>
+        <translation>Address</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(walang label)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Passphrase Dialog</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Ipasok ang passphrase</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Bagong passphrase</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Ulitin ang bagong passphrase</translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation>Ipakita ang Passphrase</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>I-encrypt ang walet.</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Kailangan ng operasyong ito ang passphrase ng iyong walet upang mai-unlock ang walet.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>I-unlock ang walet.</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Kailangan ng operasyong ito ang passphrase ng iyong walet upang ma-decrypt ang walet.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>I-decrypt ang walet.</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Baguhin ang passphrase</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Kumpirmahin ang pag-encrypt ng walet.</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Babala: Kung na-encrypt mo ang iyong walet at nawala ang iyong passphrase, &lt;b&gt;MAWAWALA MO ANG LAHAT NG IYONG MGA BITCOIN!&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Sigurado ka bang nais mong i-encrypt ang iyong walet?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Naka-encrypt ang walet.</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase for the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Ipasok ang bagong passphrase para sa wallet. (1)Mangyaring gumamit ng isang passphrase na(2) sampu o higit pang mga random na characte‭r(2), o (3)walo o higit pang mga salita(3).</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase for the wallet.</source>
+        <translation>Ipasok ang lumang passphrase at bagong passphrase para sa pitaka.</translation>
+    </message>
+    <message>
+        <source>Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>Tandaan na ang pag-encrypt ng iyong pitaka ay hindi maaaring ganap na maprotektahan ang iyong mga bitcoin mula sa pagnanakaw ng malware na nahahawa sa iyong computer.</translation>
+    </message>
+    <message>
+        <source>Wallet to be encrypted</source>
+        <translation>Ang naka-encrypt na wallet</translation>
+    </message>
+    <message>
+        <source>Your wallet is about to be encrypted. </source>
+        <translation>Malapit na ma-encrypt ang iyong pitaka.</translation>
+    </message>
+    <message>
+        <source>Your wallet is now encrypted. </source>
+        <translation>Ang iyong wallet ay naka-encrypt na ngayon.</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>MAHALAGA: Anumang nakaraang mga backup na ginawa mo sa iyong walet file ay dapat mapalitan ng bagong-buong, naka-encrypt na walet file. Para sa mga kadahilanang pangseguridad, ang mga nakaraang pag-backup ng hindi naka-encrypt na walet file ay mapagwawalang-silbi sa sandaling simulan mong gamitin ang bagong naka-encrypt na walet.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Nabigo ang pag-encrypt ng walet</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Nabigo ang pag-encrypt ng walet dahil sa isang panloob na error. Hindi na-encrypt ang iyong walet.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Ang mga ibinigay na passphrase ay hindi tumutugma.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Nabigo ang pag-unlock ng walet</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Ang passphrase na ipinasok para sa pag-decrypt ng walet ay hindi tama.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Nabigo ang pag-decrypt ng walet</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Matagumpay na nabago ang passphrase ng walet.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Babala: Ang Caps Lock key ay nakabukas!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netmask</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Bawal Hanggang</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Pirmahan ang mensahe...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Nag-sy-synchronize sa network...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>Pangkalahatang-ideya</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Ipakita ang pangkalahatan ng walet</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>Transaksyon</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>I-browse ang kasaysayan ng transaksyon</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Labasan</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Ihinto ang application</translation>
+    </message>
+    <message>
+        <source>&amp;About %1</source>
+        <translation>Mga %1</translation>
+    </message>
+    <message>
+        <source>Show information about %1</source>
+        <translation>I
