0987ef55eb89ce73217621354cd40c31622b73e5
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-11-16 21:18:35 +0000

    Updated de translation

---
+++ src/qt/locale/bitcoin_gl_ES.ts
@@ -0,0 +1,138 @@
+<TS language="gl_ES" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Fai Click co botón dereito para editar o enderezo ou etiqueta</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Crea un novo enderezo</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Novo</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Copia o enderezo seleccionado ao portapapeis do sistema</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Copiar</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>Pechar</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Borra o enderezo seleccionado actualmente da lista</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>Introduce un enderezo ou etiqueta para buscar</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Exporta os datos na pestana actual a un ficheiro</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Selecciona o enderezo ó que enviar moedas</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Selecciona o enderezo do que recibir moedas</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>Selecciona</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Enderezos de envío</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Enderezos de recepción</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Estes son os teus enderezos de Bitcoin para enviar pagamentos. Asegurate sempre de comprobar a cantidade e maila dirección antes de enviar moedas.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>Copiar Enderezo</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Copia Etiqueta</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>Edita</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Exporta a Lista de Enderezos</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Ficheiro Separado por Comas (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Exportación Fallida</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Houbo un erro tentando gardar a lista de enderezos en %1. Por favor proba de novo.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Etiqueta</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Enderezo</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(sin etiqueta)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Diálogo de Frase Contrasinal</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Introduce a frase contrasinal</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nova frase contrasinal</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Repite a frase contrasinal</translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation>Mostra frase contrasinal</translation>
+    </message>
+    <m
