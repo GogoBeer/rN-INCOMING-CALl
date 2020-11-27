ef7ddcf59662c41dce8c55ea89da3f04e9b1e80c
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-11-27 17:28:26 +0000

    Planning

--- src/qt/locale/bitcoin_hi.ts
+++ src/qt/locale/bitcoin_hi.ts
@@ -29,4 +29,183 @@
         <source>Delete the currently selected address from the list</source>
         <translation>चुने हुए एड्रेस को सूची से हटाएं</translation>
     </message>
-    <messa
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>ढूंढने के लिए एड्रेस या लेबल दर्ज करें</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>डेटा को मौजूदा टैब से एक फ़ाइल में निर्यात करें</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;निर्यात</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;मिटाए</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>कॉइन भेजने के लिए एड्रेस चुनें</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>कॉइन प्राप्त करने के लिए एड्रेस चुनें </translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;चुनें</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>एड्रेस भेजे जा रहें हैं</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>एड्रेस प्राप्त किए जा रहें हैं</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>भुगतान करने के लिए ये आपके बिटकॉइन एड्रेस हैं। कॉइन भेजने से पहले राशि और गंतव्य एड्रेस की हमेशा जाँच करें </translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;एड्रेस कॉपी करें</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>कॉपी &amp;लेबल </translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;बदलाव करें</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>एड्रेस की सूची निर्यात करें</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>कौमा सेपरेटेड फाइल (* .csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>निर्यात विफल रहा</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>एड्रेस की सूची को %1 में सहेजने का प्रयास करने में त्रुटि हुई। कृपया पुन: प्रयास करें।</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>लेबल</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>एड्रेस</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(लेबल नहीं है)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>पासफ़्रेज़ डायलॉग</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>पासफ्रेज़ दर्ज करें</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>नया पासफ्रेज़</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>नया पासफ्रेज़ दोबारा दर्ज करें </translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation>पासफ्रेज़ उजागर करे</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>वॉलेट एन्क्रिप्ट करें</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>इस संचालान हेतु कृपया अपने वॉलेट के सुरक्षा संवाद को दर्ज करें</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>बटुए को अनलॉक करें</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>आपके वॉलेट को गोपनीय बनाने हेतु आपके वॉलेट का सुरक्षा संवाद अनिवार्य है</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>वॉलेट को डिक्रिप्ट करें</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>पासफ़्रेज़ बदलें</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>वॉलेट एन्क्रिप्शन की पुष्टि करें</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>चेतावनी: यदि आपने वॉलेट को एन्क्रिप्ट करने के बाद पदबंध खो दी तो &lt;b&gt;आप सारे बिटकॉइन खो देंगे &lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>क्या आप सुनिश्चित हैं कि आप अपने वॉलेट को एन्क्रिप्ट करना चाहते हैं ?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>वॉलेट को एन्क्रिप्ट किया गया है</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase for the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>वॉलेट में नया सुरक्षा संवाद दर्ज करें | कृपया दस या उससे अधिक, या फिर आठ या उससे अधिक अव्यवस्थित अंको से ही अपना सुरक्षा संवाद बनाएं ।</translation>
+    </message>
+    <message>
+        <source>Enter the old passphrase and new passphrase for the wallet.</source>
+        <translation>वॉलेट में पुराना एवं नया सुरक्षा संवाद दर्ज करें ।</translation>
+    </message>
+    <message>
+        <source>Remember that encrypting your wallet cannot fully protect your bitcoins from being stolen by malware infecting your computer.</source>
+        <translation>याद रखें कि अपने बटुए (वॉलेट) एन्क्रिप्ट करना आपके कंप्यूटर को संक्रमित करने वाले मैलवेयर द्वारा आपके बिटकॉइन को चोरी होने से पूरी तरह से सुरक्षित नहीं कर सकता है।</translation>
+    </message>
+    <message>
+        <source>Wallet to be encrypted</source>
+        <translation>बटुए "वॉलेट" को एन्क्रिप्ट किया जाना है</translation>
+    </message>
+    <message>
+        <source>Your wallet is about to be encrypted. </source>
+        <translation>आपका बटुआ "वॉलेट" एन्क्रिप्टेड होने वाला है।</translation>
+    </message>
+    <message>
+        <source>Your wallet is now encrypted. </source>
+        <translation>आपका बटुआ "वॉलेट" एन्क्रिप्ट हो गया है।</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>महत्वपूर्ण: किसी भी पिछले बैकअप को आपने अपनी वॉलेट फ़ाइल से बनाया था, उसे नए जनरेट किए गए एन्क्रिप्टेड वॉलेट फ़ाइल से बदल दिया जाना चाहिए। सुरक्षा कारणों से, अनएन्क्रिप्टेड वॉलेट फ़ाइल के पिछले बैकअप बेकार हो जाएंगे जैसे ही आप नए, एन्क्रिप्टेड वॉलेट का उपयोग करना शुरू करते हैं।</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>वॉलेट एन्क्रिप्शन विफल रहा</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.
