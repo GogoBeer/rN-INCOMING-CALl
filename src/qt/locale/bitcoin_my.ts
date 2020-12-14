2dda3c35b3b3c2323fa9708f5c8f456a69c8aa8a
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-12-14 12:41:49 +0000

    Update roadmap.

--- src/qt/locale/bitcoin_my.ts
+++ src/qt/locale/bitcoin_my.ts
@@ -92,4 +92,102 @@
     </context>
 <context>
     <name>OpenURIDialog</name>
-  
+    </context>
+<context>
+    <name>OpenWalletActivity</name>
+    </context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>အမှား</translation>
+    </message>
+    </context>
+<context>
+    <name>OverviewPage</name>
+    </context>
+<context>
+    <name>PSBTOperationsDialog</name>
+    </context>
+<context>
+    <name>PaymentServer</name>
+    </context>
+<context>
+    <name>PeerTableModel</name>
+    </context>
+<context>
+    <name>QObject</name>
+    </context>
+<context>
+    <name>QRImageWidget</name>
+    </context>
+<context>
+    <name>RPCConsole</name>
+    </context>
+<context>
+    <name>ReceiveCoinsDialog</name>
+    </context>
+<context>
+    <name>ReceiveRequestDialog</name>
+    </context>
+<context>
+    <name>RecentRequestsTableModel</name>
+    </context>
+<context>
+    <name>SendCoinsDialog</name>
+    </context>
+<context>
+    <name>SendCoinsEntry</name>
+    </context>
+<context>
+    <name>ShutdownWindow</name>
+    </context>
+<context>
+    <name>SignVerifyMessageDialog</name>
+    </context>
+<context>
+    <name>TrafficGraphWidget</name>
+    </context>
+<context>
+    <name>TransactionDesc</name>
+    </context>
+<context>
+    <name>TransactionDescDialog</name>
+    </context>
+<context>
+    <name>TransactionTableModel</name>
+    </context>
+<context>
+    <name>TransactionView</name>
+    </context>
+<context>
+    <name>UnitDisplayStatusBarControl</name>
+    </context>
+<context>
+    <name>WalletController</name>
+    </context>
+<context>
+    <name>WalletFrame</name>
+    </context>
+<context>
+    <name>WalletModel</name>
+    </context>
+<context>
+    <name>WalletView</name>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;ထုတ်ယူသိမ်းဆည်း</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>လက်ရှိ tab မှာရှိတဲ့ဒေတာတွေကို ဖိုင်လ်မှာသိမ်းမယ်။</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>အမှား</translation>
+    </message>
+    </context>
+<context>
+    <name>bitcoin-core</name>
+    </context>
+</TS>
---
+++ src/qt/locale/bitcoin_nb.ts
@@ -0,0 +1,161 @@
+<TS language="nb" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Høyreklikk for å redigere adressen eller beskrivelsen</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Opprett en ny adresse</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Ny</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopier den valgte adressen til utklippstavlen</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopier</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>&amp;Lukk</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Slett den valgte adressen fra listen</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>Oppgi adresse, eller stikkord, for å søke</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Eksporter data i den valgte fliken til en fil</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Eksport</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Slett</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Velg en adresse å sende mynter til</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Velg adressen som skal motta myntene</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;Velg</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Avsender adresser</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Mottager adresser</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Dette er dine Bitcoin adresser for å sende  å sende betalinger. Husk å sjekke beløp og mottager adresser før du sender mynter.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopier adresse</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopier &amp;beskrivelse</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>R&amp;ediger</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Eksporter adresse listen</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Komma separert fil (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Eksporten feilet</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Fet oppstod en feil ved lagring av adresselisten til %1. Vennligst prøv igjen.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Beskrivelse</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(ingen beskrivelse)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Passord dialog</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Oppgi passord setning</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Ny passord setning</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Repeter passorsetningen</translation>
+    </message>
+    <message>
+        <source>Show passphrase</source>
+        <translation>Vis adgangsfrase</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Krypter lommeboken</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Denne operasjonen krever passordsetningen for å låse opp lommeboken.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Lås opp lommeboken</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Denne operasjonen krever passordsetningen for å dekryptere lommeboken.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Dekrypter lommeboken</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Endre passordsetningen</translation>
+ 
