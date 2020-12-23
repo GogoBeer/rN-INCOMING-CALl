264cfcff01b030c98d1d7f0e46462993af30a202
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2020-12-23 11:47:38 +0000

    Update _ml-dataangel.html

---
+++ src/qt/locale/bitcoin_ro.ts
@@ -0,0 +1,41 @@
+<TS language="ro" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Click-dreapta pentru a edita adresa sau eticheta</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Creează o adresă nouă</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nou</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Copiază adresa selectată curent în clipboard</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Copiază</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>Î&amp;nchide</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Şterge adresa selectată curent din listă</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>Introduceţi adresa sau eticheta pentru căutare</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Exportă datele din tab-ul curent într-un fişier</translation>
+    </message>
+    <message>
+        <sou
