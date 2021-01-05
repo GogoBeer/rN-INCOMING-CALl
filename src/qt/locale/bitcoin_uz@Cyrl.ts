43d64d2ee68dcb0cd33df70124ba6bcf2cf8b01b
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2021-01-05 16:14:21 +0000

    remove unnecessary feeds

---
+++ src/qt/locale/bitcoin_uz@Cyrl.ts
@@ -0,0 +1,98 @@
+<TS language="uz@Cyrl" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Манзил ёки ёрлиқни таҳрирлаш учун икки марта босинг</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Янги манзил яратинг</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Янги</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Жорий танланган манзилни тизим вақтинчалик хотирасига нусха кўчиринг</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Нусха олиш</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>&amp;Ёпиш</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Жорий танланган манзилни рўйхатдан ўчириш</translation>
+    </message>
+    <message>
+        <source>Enter address or label to search</source>
+        <translation>Излаш учун манзил ёки ёрлиқни киритинг</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Жорий ички ойна ичидаги маълумотларни файлга экспорт қилиш</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Экспорт</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Ўчириш</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Тангаларни жўнатиш учун манзилни танланг</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Тангаларни қабул қилиш учун манзилни танланг</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;Танлаш</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Жўнатиладиган манзиллар</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Қабул қилинадиган манзиллар</translation>
+    </message>
+    <message>
+        <source>These are your Bitcoin addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Улар тўловларни жўнатиш учун сизнинг Bitcoin манзилларингиз. Доимо тангаларни жўнатишдан олдин сумма ва қабул қилувчи манзилни текшириб кўринг. </translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>Манзилдан &amp;нусха олиш</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Нусха олиш ва ёрлиқ</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Таҳрирлаш</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Манзил рўйхатини экспорт қилиш</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Вергул билан ажратилган файл (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Экспорт қилиб бўлмади</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Манзил рўйхатини %1.га сақлашда хатолик 
