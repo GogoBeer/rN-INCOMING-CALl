6ba63c1335f2db1cd9e89e221cd546fed9f1aa2a
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2021-01-07 05:47:00 +0000

    Fixed package version

--- src/qt/locale/bitcoin_uz@Cyrl.ts
+++ src/qt/locale/bitcoin_uz@Cyrl.ts
@@ -95,4 +95,205 @@
     </message>
     <message>
         <source>There was an error trying to save the address list to %1. Please try again.</source>
-        <translation>Манзил рўйхатини %1.га сақлашда хатолик 
+        <translation>Манзил рўйхатини %1.га сақлашда хатолик юз берди. Яна уриниб кўринг.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Ёрлиқ</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Манзил</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(Ёрлиқ мавжуд эмас)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Махфий сўз ойнаси</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Махфий сузни киритинг</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Янги махфий суз</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Янги махфий сузни такрорланг</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Ҳамённи қодлаш</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Ушбу операцияни амалга ошириш учун ҳамённи қулфдан чиқариш парол сўзини талаб қилади.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Ҳамённи қулфдан чиқариш</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Ушбу операцияни амалга ошириш учун ҳамённи коддан чиқариш парол сўзини талаб қилади.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Ҳамённи коддан чиқариш</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Махфий сузни узгартириш</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Ҳамённи кодлашни тасдиқлаш</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR BITCOINS&lt;/b&gt;!</source>
+        <translation>Диққат: Агар сиз ҳамёнингизни кодласангиз ва махфий сўзингизни унутсангиз, сиз &lt;b&gt;БАРЧА BITCOIN ПУЛЛАРИНГИЗНИ ЙЎҚОТАСИЗ&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Ҳамёнингизни кодлашни ростдан хоҳлайсизми?</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Ҳамёни кодланган</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>МУҲИМ: Сиз қилган олдинги ҳамён файли заҳиралари янги яратилган, кодланган ҳамён файли билан алмаштирилиши керак. Хавфсизлик сабабларига кўра олдинги кодланган ҳамён файли заҳираси янги кодланган ҳамёндан фойдаланишингиз билан яроқсиз ҳолга келади.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Ҳамённи кодлаш амалга ошмади</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Ҳамённи кодлаш ташқи хато туфайли амалга ошмади. Ҳамёнингиз кодланмади.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Киритилган пароллар мос келмади.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Ҳамённи қулфдан чиқариш амалга ошмади</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Ҳамённи коддан чиқариш учун киритилган парол нотўғри.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Ҳамённи коддан чиқариш амалга ошмади</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Ҳамён пароли муваффақиятли алмаштирилди.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Диққат: Caps Lock тугмаси ёқилган!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    </context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>&amp;Хабар ёзиш...</translation>
+    </message>
+    <message>
+        <source>Synchronizing with network...</source>
+        <translation>Тармоқ билан синхронланмоқда...</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Кўриб чиқиш</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Ҳамённинг умумий кўринишини кўрсатиш</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Пул ўтказмалари</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Пул ўтказмалари тарихини кўриш</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Ч&amp;иқиш</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Иловадан чиқиш</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>&amp;Qt ҳақида</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Qt ҳақидаги маълумотларни кўрсатиш</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;Мосламалар...</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>Ҳамённи &amp;кодлаш...</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>Ҳамённи &amp;заҳиралаш...</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Махфий сўзни &amp;ўзгартириш...</translation>
+    </message>
+    <message>
+        <source>Open &amp;URI...</source>
+        <translation>Интернет манзилни очиш</translation>
+    </message>
+    <message>
+        <source>Reindexing blocks on disk...</source>
+        <translation>Дискдаги блоклар қайта индексланмоқда...</translation>
+    </message>
+    <message>
+        <source>Send coins to a Bitcoin address</source>
+        <translation>Тангаларни Bitcoin манзилига жўнатиш</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Ҳамённи бошқа манзилга заҳиралаш</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>Паролни ўзгартириш ҳамённи кодлашда фойдаланилади</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>Хабарни &amp;тасдиқлаш...</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;Жўнатиш</translation>
+    </mes
