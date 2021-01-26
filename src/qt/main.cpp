df71736867e81652f991dfee40e67b507be6dca0
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2021-01-26 17:20:05 +0000

    add background color

---
+++ src/qt/main.cpp
@@ -0,0 +1,57 @@
+
+// Copyright (c) 2018-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifdef HAVE_CONFIG_H
+#include <config/bitcoin-config.h>
+#endif
+
+#if USE_QML
+#include <qml/bitcoin.h>
+#else
+#include <qt/bitcoin.h>
+#endif // USE_QML
+
+#include <interfaces/node.h>
+#include <noui.h>
+#include <util/translation.h>
+#include <util/url.h>
+
+#include <QCoreApplication>
+
+#include <functional>
+#include <string>
+
+#if defined(QT_STATICPLUGIN)
+#include <QtPlugin>
+#if defined(QT_QPA_PLATFORM_XCB)
+Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_WINDOWS)
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_COCOA)
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_ANDROID)
+Q_IMPORT_PLUGIN(QAndroidPlatformIntegrationPlugin)
+#endif
+#endif
+
+/** Translate string to current locale using Qt. */
+extern const std::function<std::string(const char*)> G_TRANSLATION_FUN = [](const char* psz) {
+    return QCoreApplication::translate("bitcoin-core", psz).toStdString();
+};
+UrlDecodeFn* const URL_DECODE = urlDecode;
+
+int main(int argc, char* argv[])
+{
+    qRegisterMetaType<interfaces::BlockAndHeaderTipInfo>("interfaces::BlockAndHeaderTipInfo");
+
+    // Subscribe to global signals from core.
+    noui_connect();
+
+#if USE_QML
+    return QmlGuiMain(argc, argv);
+#else
+    return GuiMain(argc, argv);
+#endif // USE_QML
+}
---
+++ src/qt/modaloverlay.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2016-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_MODALOVERLAY_H
+#define BITCOIN_QT_MODALOVERLAY_H
+
+#include <QDateTime>
+#include <QPropertyAnimation>
+#include <QWidget>
+
+//! The required delta of headers to the estimated number of available headers until we show the IBD progress
+static constexpr int HEADER_HEIGHT_DELTA_SYNC = 24;
+
+namespace Ui {
+    class ModalOverlay;
+}
+
+/** Modal overlay to display information about the chain-sync state */
+class ModalOverlay : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ModalOverlay(bool enable_wallet, QWidget *parent);
+    ~ModalOverlay();
+
+    void tipUpdate(int count, const QDateTime& blockDate, double nVerification
