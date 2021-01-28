066c4f60cb7d7f09fe95b80209df22a46579da2a
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2021-01-28 05:46:49 +0000

    [INTERNAL][TEST] sap.tnt ToolPage: reference images changed

--- src/qt/paymentserver.h
+++ src/qt/paymentserver.h
@@ -41,4 +41,69 @@
 #include <QObject>
 #include <QString>
 
-class Optio
+class OptionsModel;
+
+namespace interfaces {
+class Node;
+} // namespace interfaces
+
+QT_BEGIN_NAMESPACE
+class QApplication;
+class QByteArray;
+class QLocalServer;
+class QUrl;
+QT_END_NAMESPACE
+
+class PaymentServer : public QObject
+{
+    Q_OBJECT
+
+public:
+    // Parse URIs on command line
+    // Returns false on error
+    static void ipcParseCommandLine(int argc, char *argv[]);
+
+    // Returns true if there were URIs on the command line
+    // which were successfully sent to an already-running
+    // process.
+    // Note: if a payment request is given, SelectParams(MAIN/TESTNET)
+    // will be called so we startup in the right mode.
+    static bool ipcSendCommandLine();
+
+    // parent should be QApplication object
+    explicit PaymentServer(QObject* parent, bool startLocalServer = true);
+    ~PaymentServer();
+
+    // OptionsModel is used for getting proxy settings and display unit
+    void setOptionsModel(OptionsModel *optionsModel);
+
+Q_SIGNALS:
+    // Fired when a valid payment request is received
+    void receivedPaymentRequest(SendCoinsRecipient);
+
+    // Fired when a message should be reported to the user
+    void message(const QString &title, const QString &message, unsigned int style);
+
+public Q_SLOTS:
+    // Signal this when the main window's UI is ready
+    // to display payment requests to the user
+    void uiReady();
+
+    // Handle an incoming URI, URI with local file scheme or file
+    void handleURIOrFile(const QString& s);
+
+private Q_SLOTS:
+    void handleURIConnection();
+
+protected:
+    // Constructor registers this on the parent QApplication to
+    // receive QEvent::FileOpen and QEvent:Drop events
+    bool eventFilter(QObject *object, QEvent *event) override;
+
+private:
+    bool saveURIs;                      // true during startup
+    QLocalServer* uriServer;
+    OptionsModel *optionsModel;
+};
+
+#endif // BITCOIN_QT_PAYMENTSERVER_H
