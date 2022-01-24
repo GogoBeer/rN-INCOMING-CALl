287d5899e00d61bc2a1d42c925609ea61faeef89
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2022-01-24 11:34:49 +0000

    hugetlb: do not use vma_hugecache_offset() for vma_prio_tree_foreach

--- src/test/fuzz/net_permissions.cpp
+++ src/test/fuzz/net_permissions.cpp
@@ -21,3 +21,21 @@ FUZZ_TARGET(net_permissions)
 
     NetWhitebindPermissions net_whitebind_permissions;
     bilingual_str error_net_whitebind_permissions;
+    if (NetWhitebindPermissions::TryParse(s, net_whitebind_permissions, error_net_whitebind_permissions)) {
+        (void)NetPermissions::ToStrings(net_whitebind_permissions.m_flags);
+        (void)NetPermissions::AddFlag(net_whitebind_permissions.m_flags, net_permission_flags);
+        assert(NetPermissions::HasFlag(net_whitebind_permissions.m_flags, net_permission_flags));
+        (void)NetPermissions::ClearFlag(net_whitebind_permissions.m_flags, NetPermissionFlags::Implicit);
+        (void)NetPermissions::ToStrings(net_whitebind_permissions.m_flags);
+    }
+
+    NetWhitelistPermissions net_whitelist_permissions;
+    bilingual_str error_net_whitelist_permissions;
+    if (NetWhitelistPermissions::TryParse(s, net_whitelist_permissions, error_net_whitelist_permissions)) {
+        (void)NetPermissions::ToStrings(net_whitelist_permissions.m_flags);
+        (void)NetPermissions::AddFlag(net_whitelist_permissions.m_flags, net_permission_flags);
+        assert(NetPermissions::HasFlag(net_whitelist_permissions.m_flags, net_permission_flags));
+        (void)NetPermissions::ClearFlag(net_whitelist_permissions.m_flags, NetPermissionFlags::Implicit);
+        (void)NetPermissions::ToStrings(net_whitelist_permissions.m_flags);
+    }
+}
