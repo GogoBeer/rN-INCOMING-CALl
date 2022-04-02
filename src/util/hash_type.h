4172dea9ab013938d2ec7fdc9a3c6a4b77fe85b7
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2022-04-02 18:44:18 +0000

    WL#7777 Integrate PFS memory instrumentation with InnoDB

---
+++ src/util/hash_type.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_UTIL_HASH_TYPE_H
+#define BITCOIN_UTIL_HASH_TYPE_H
+
+template <typename HashType>
+class BaseHash
+{
+protected:
+    HashType m_hash
