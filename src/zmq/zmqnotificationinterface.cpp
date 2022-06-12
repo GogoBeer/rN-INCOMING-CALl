6d903f2ddc8374635f476895be681fd5a43a7c1b
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2022-06-12 09:25:47 +0000

    ALSA: usb-audio: work around Android accessory firmware bug

---
+++ src/zmq/zmqnotificationinterface.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2015-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <zmq/zmqnotificationinterface.h>
+#include <zmq/zmqpublishnotifier.h>
+#include <zmq/zmqutil.h>
+
+#include <zmq.h>
+
+#include <validation.h>
+#include <util/system.h>
+
+CZMQNotificationInterface::CZMQNotificationInterface() : pcontext(nullptr)
+{
+}
+
+CZMQNotificationInterface::~CZMQNotificationInterface()
+{
+    Shutdown();
+}
+
+std::list<const CZMQAbstractNotifier*> CZMQNotificationInterface::GetActiveNotifiers() const
+{
+    std::list<const CZMQAbstractNotifier*> result;
+    for (const auto& n : notifiers) {
+        result.push_back(n.get());
+    }
+    return result;
+}
+
+CZMQNotificationInterface* CZMQNotificationInterface::Create()
+{
+    std::map<std::string, CZMQNotifierFactory> factories;
+    factories["pubhashblock"] = CZMQAbstractNotifier::Create<CZMQPublishHashBlockNotifier>;
+    factories["pubhashtx"] = CZMQAbstract
