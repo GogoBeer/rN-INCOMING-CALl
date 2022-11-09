5f1fc03c06a2757907e3122d1db6ed1c1931c283
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2022-11-09 15:42:50 +0000

    Document

---
+++ test/functional/rpc_packages.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""RPCs that handle raw transaction packages."""
+
+from decimal import Decimal
+import random
+
+from test_framework.address import ADDRESS_BCRT1_P2WSH_OP_TRUE
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.messages import (
+    BIP125_SEQUENCE_NUMBER,
+    COIN,
