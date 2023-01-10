d3f15cff51577589e609431da11f2fba670a1c0f
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2023-01-10 13:58:19 +0000

    Add authentication

--- test/functional/wallet_importmulti.py
+++ test/functional/wallet_importmulti.py
@@ -12,4 +12,183 @@ variants.
   return the privkeys, pubkeys and all variants of scriptPubKey and address.
 - `test_importmulti()` is called to send an importmulti call to node1, test
   success, and (if unsuccessful) test the error code and error message returned.
-- `test_addres
+- `test_address()` is called to call getaddressinfo for an address on node1
+  and test the values returned."""
+
+from test_framework.blocktools import COINBASE_MATURITY
+from test_framework.script import (
+    CScript,
+    OP_NOP,
+)
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.descriptors import descsum_create
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than,
+    assert_raises_rpc_error,
+)
+from test_framework.wallet_util import (
+    get_key,
+    get_multisig,
+    test_address,
+)
+
+
+class ImportMultiTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 2
+        self.extra_args = [["-addresstype=legacy"], ["-addresstype=legacy"]]
+        self.setup_clean_chain = True
+
+    def skip_test_if_missing_module(self):
+        self.skip_if_no_wallet()
+
+    def setup_network(self):
+        self.setup_nodes()
+
+    def test_importmulti(self, req, success, error_code=None, error_message=None, warnings=None):
+        """Run importmulti and assert success"""
+        if warnings is None:
+            warnings = []
+        result = self.nodes[1].importmulti([req])
+        observed_warnings = []
+        if 'warnings' in result[0]:
+            observed_warnings = result[0]['warnings']
+        assert_equal("\n".join(sorted(warnings)), "\n".join(sorted(observed_warnings)))
+        assert_equal(result[0]['success'], success)
+        if error_code is not None:
+            assert_equal(result[0]['error']['code'], error_code)
+            assert_equal(result[0]['error']['message'], error_message)
+
+    def run_test(self):
+        self.log.info("Mining blocks...")
+        self.generate(self.nodes[0], 1, sync_fun=self.no_op)
+        self.generate(self.nodes[1], 1, sync_fun=self.no_op)
+        timestamp = self.nodes[1].getblock(self.nodes[1].getbestblockhash())['mediantime']
+
+        node0_address1 = self.nodes[0].getaddressinfo(self.nodes[0].getnewaddress())
+
+        # Check only one address
+        assert_equal(node0_address1['ismine'], True)
+
+        # Node 1 sync test
+        assert_equal(self.nodes[1].getblockcount(), 1)
+
+        # Address Test - before import
+        address_info = self.nodes[1].getaddressinfo(node0_address1['address'])
+        assert_equal(address_info['iswatchonly'], False)
+        assert_equal(address_info['ismine'], False)
+
+        # RPC importmulti -----------------------------------------------
+
+        # Bitcoin Address (implicit non-internal)
+        self.log.info("Should import an address")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": {"address": key.p2pkh_addr},
+                               "timestamp": "now"},
+                              success=True)
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=True,
+                     ismine=False,
+                     timestamp=timestamp,
+                     ischange=False)
+        watchonly_address = key.p2pkh_addr
+        watchonly_timestamp = timestamp
+
+        self.log.info("Should not import an invalid address")
+        self.test_importmulti({"scriptPubKey": {"address": "not valid address"},
+                               "timestamp": "now"},
+                              success=False,
+                              error_code=-5,
+                              error_message='Invalid address \"not valid address\"')
+
+        # ScriptPubKey + internal
+        self.log.info("Should import a scriptPubKey with internal flag")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": key.p2pkh_script,
+                               "timestamp": "now",
+                               "internal": True},
+                              success=True)
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=True,
+                     ismine=False,
+                     timestamp=timestamp,
+                     ischange=True)
+
+        # ScriptPubKey + internal + label
+        self.log.info("Should not allow a label to be specified when internal is true")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": key.p2pkh_script,
+                               "timestamp": "now",
+                               "internal": True,
+                               "label": "Unsuccessful labelling for internal addresses"},
+                              success=False,
+                              error_code=-8,
+                              error_message='Internal addresses should not have a label')
+
+        # Nonstandard scriptPubKey + !internal
+        self.log.info("Should not import a nonstandard scriptPubKey without internal flag")
+        nonstandardScriptPubKey = key.p2pkh_script + CScript([OP_NOP]).hex()
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": nonstandardScriptPubKey,
+                               "timestamp": "now"},
+                              success=False,
+                              error_code=-8,
+                              error_message='Internal must be set to true for nonstandard scriptPubKey imports.')
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=False,
+                     ismine=False,
+                     timestamp=None)
+
+        # Address + Public key + !Internal(explicit)
+        self.log.info("Should import an address with public key")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": {"address": key.p2pkh_addr},
+                               "timestamp": "now",
+                               "pubkeys": [key.pubkey],
+                               "internal": False},
+                              success=True,
+                              warnings=["Some private keys are missing, outputs will be considered watchonly. If this is intentional, specify the watchonly flag."])
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=True,
+                     ismine=False,
+                     timestamp=timestamp)
+
+        # ScriptPubKey + Public key + internal
+        self.log.info("Should import a scriptPubKey with internal and with public key")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": key.p2pkh_script,
+                               "timestamp": "now",
+                               "pubkeys": [key.pubkey],
+                               "internal": True},
+                              success=True,
+                              warnings=["Some private keys are missing, outputs will be considered watchonly. If this is intentional, specify the watchonly flag."])
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=True,
+                     ismine=False,
+                     timestamp=timestamp)
+
+        # Nonstandard scriptPubKey + Public key + !internal
+        self.log.info("Should not import a nonstandard scriptPubKey without internal and with public key")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPubKey": nonstandardScriptPubKey,
+                               "timestamp": "now",
+                               "pubkeys": [key.pubkey]},
+                              success=False,
+                              error_code=-8,
+                              error_message='Internal must be set to true for nonstandard scriptPubKey imports.')
+        test_address(self.nodes[1],
+                     key.p2pkh_addr,
+                     iswatchonly=False,
+                     ismine=False,
+                     timestamp=None)
+
+        # Address + Private key + !watchonly
+        self.log.info("Should import an address with private key")
+        key = get_key(self.nodes[0])
+        self.test_importmulti({"scriptPu
