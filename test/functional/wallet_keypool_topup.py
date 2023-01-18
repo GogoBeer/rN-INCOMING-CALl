a3ae69785c67f3dd5090269845d6ab0217ea39cf
GogoBeer <101608499+GogoBeer@users.noreply.github.com>
2023-01-18 21:13:13 +0000

    Some Improvements

--- test/functional/wallet_keypool_topup.py
+++ test/functional/wallet_keypool_topup.py
@@ -67,4 +67,27 @@ class KeypoolRestoreTest(BitcoinTestFramework):
             self.nodes[0].sendtoaddress(addr_extpool, 5)
             self.generate(self.nodes[0], 1)
 
-            self.log.info("Restart nod
+            self.log.info("Restart node with wallet backup")
+            self.stop_node(idx)
+            shutil.copyfile(wallet_backup_path, wallet_path)
+            self.start_node(idx, self.extra_args[idx])
+            self.connect_nodes(0, idx)
+            self.sync_all()
+
+            self.log.info("Verify keypool is restored and balance is correct")
+            assert_equal(self.nodes[idx].getbalance(), 15)
+            assert_equal(self.nodes[idx].listtransactions()[0]['category'], "receive")
+            # Check that we have marked all keys up to the used keypool key as used
+            if self.options.descriptors:
+                if output_type == 'legacy':
+                    assert_equal(self.nodes[idx].getaddressinfo(self.nodes[idx].getnewaddress(address_type=output_type))['hdkeypath'], "m/44'/1'/0'/0/110")
+                elif output_type == 'p2sh-segwit':
+                    assert_equal(self.nodes[idx].getaddressinfo(self.nodes[idx].getnewaddress(address_type=output_type))['hdkeypath'], "m/49'/1'/0'/0/110")
+                elif output_type == 'bech32':
+                    assert_equal(self.nodes[idx].getaddressinfo(self.nodes[idx].getnewaddress(address_type=output_type))['hdkeypath'], "m/84'/1'/0'/0/110")
+            else:
+                assert_equal(self.nodes[idx].getaddressinfo(self.nodes[idx].getnewaddress(address_type=output_type))['hdkeypath'], "m/0'/0'/110'")
+
+
+if __name__ == '__main__':
+    KeypoolRestoreTest().main()
